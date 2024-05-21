7a3a7645db50f631fbf4c043b62b1a49315288e3
hendrik_1409 <hendrik.schiele@gmx.de>
2024-05-21 18:40:31 +0200

    implemented init and call of print

--- Blatt04/src/Board.java
+++ Blatt04/src/Board.java
@@ -1,89 +1,143 @@
 import java.util.InputMismatchException;
+import java.util.LinkedList;
 import java.util.Stack;
 
 import static java.lang.Math.abs;
+
 /**
  * This class represents a generic TicTacToe game board.
  */
 public class Board {
     private int n;
-    //TODO
-    
+    private int[][] board;
+    private int freeFields;
+    private Position lastMove;
+
     /**
-     *  Creates Board object, am game board of size n * n with 1<=n<=10.
+     * Creates Board object, am game board of size n * n with 1<=n<=10.
      */
-    public Board(int n)
-    {
-        // TODO
+    public Board(int n) throws InputMismatchException {
+        this.n = n;
+        if (1 > this.n || this.n > 10) {
+            throw new InputMismatchException();
+        }
+        this.freeFields = n * n;
+        this.board = new int[n][n];
     }
-    
+
     /**
-     *  @return     length/width of the Board object
+     * @return length/width of the Board object
      */
-    public int getN() { return n; }
-    
+    public int getN() {
+        return n;
+    }
+
     /**
-     *  @return     number of currently free fields
+     * @return number of currently free fields
      */
     public int nFreeFields() {
-        // TODO
-    }
-    
-    /**
-     *  @return     token at position pos
-     */
-    public int getField(Position pos) throws InputMismatchException
-    {
-        // TODO
+        return this.freeFields;
     }
 
     /**
-     *  Sets the specified token at Position pos.
-     */    
-    public void setField(Position pos, int token) throws InputMismatchException
-    {
-        // TODO
-    }
-    
-    /**
-     *  Places the token of a player at Position pos.
+     * @return token at position pos
      */
-    public void doMove(Position pos, int player)
-    {
-        // TODO
+    public int getField(Position pos) throws InputMismatchException {
+        if (pos.x < 0 || pos.y < 0 || pos.x >= this.n || pos.y >= this.n) {
+            throw new InputMismatchException();
+        }
+        return this.board[pos.x][pos.y];
     }
 
     /**
-     *  Clears board at Position pos.
+     * Sets the specified token at Position pos.
      */
-    public void undoMove(Position pos)
-    {
-        // TODO
+    public void setField(Position pos, int token) throws InputMismatchException {
+        if (pos.x < 0 || pos.y < 0 || pos.x >= this.n || pos.y >= this.n || (token != -1 && token != 1 && token != 0)) {
+            throw new InputMismatchException();
+        }
+        this.board[pos.x][pos.y] = token;
+        this.freeFields--;
     }
-    
+
     /**
-     *  @return     true if game is won, false if not
+     * Places the token of a player at Position pos.
+     */
+    public void doMove(Position pos, int player) throws Exception {
+        if (this.board[pos.x][pos.y] != 0) {
+            throw new Exception("Position is not free");
+        }
+        this.setField(pos, player);
+        this.lastMove = pos;
+    }
+
+    /**
+     * Clears board at Position pos.
+     */
+    public void undoMove(Position pos) {
+        this.setField(pos, 0);
+    }
+
+    /**
+     * @return true if game is won, false if not
      */
     public boolean isGameWon() {
-        // TODO
+        int player = this.board[this.lastMove.x][this.lastMove.y];
+        boolean r0 = true;
+        boolean r1 = true;
+        boolean r2 = true;
+        boolean r3 = true;
+        for (int i = 0; i < this.n; i++) {
+            if (this.board[i][lastMove.y] != player) {
+                r0 = false;
+            }
+            if (this.board[lastMove.x][i] != player) {
+                r1 = false;
+            }
+            if (this.board[i][i] != player) {
+                r2 = false;
+            }
+            if (this.board[i][this.n - i - 1] != player) {
+                r3 = false;
+            }
+        } 
+        return (r0 || r1 || r2 || r3);
     }
 
     /**
-     *  @return     set of all free fields as some Iterable object
+     * @return set of all free fields as some Iterable object
      */
-    public Iterable<Position> validMoves()
-    {
-        // TODO
+    public Iterable<Position> validMoves() {
+        LinkedList<Position> positions = new LinkedList<Position>();
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board[i].length; j++) {
+                if (this.board[i][j] == 0) {
+                    positions.addLast(new Position(i, j));
+                }
+            }
+        }
+        return positions;
     }
 
     /**
-     *  Outputs current state representation of the Board object.
-     *  Practical for debugging.
+     * Outputs current state representation of the Board object.
+     * Practical for debugging.
      */
-    public void print()
-    {
-        // TODO
+    public void print() {
+        System.out.println("This Board:");
+        System.out.print("y  ");
+        for (int i = 0; i < this.n; i++) {
+            System.out.print((i + 1) + "  ");
+        }
+        System.out.print("\n");
+        for (int y = 0; y < n; y++) {
+            System.out.print((y + 1) + ": ");
+            for (int x = 0; x < n; x++) {
+                System.out.print(this.board[x][y] + "  ");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
     }
 
 }
-
