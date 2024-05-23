057e42b3f31ed37ac98bae101528643a00557725
hendrik_1409 <hendrik.schiele@gmx.de>
2024-05-23 08:26:10 +0200

    fixed wrong count down of freeFields in setField()

--- Blatt04/src/Board.java
+++ Blatt04/src/Board.java
@@ -56,7 +56,7 @@ public class Board {
         if (pos.x < 0 || pos.y < 0 || pos.x >= this.n || pos.y >= this.n || (token != -1 && token != 1 && token != 0)) {
             throw new InputMismatchException();
         }
-        if (token != this.board[pos.x][pos.y] && token != 0) {
+        if (token != this.board[pos.x][pos.y] && token != 0 && this.board[pos.x][pos.y] != 0) {
             this.freeFields--;
         }
         else if (token != this.board[pos.x][pos.y] && token == 0) {
