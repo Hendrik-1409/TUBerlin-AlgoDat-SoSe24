9532694f6a1b7c9b7298c2404bcacced1da133cd
hendrik_1409 <hendrik.schiele@gmx.de>
2024-05-30 13:39:37 +0200

    maxGainRecursive

--- Blatt05/src/GenomicsTest.java
+++ Blatt05/src/GenomicsTest.java
@@ -1,21 +1,51 @@
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 class GenomicsTest {
+    @ParameterizedTest
+    @MethodSource("simpleTestSource")
+    void optBottomUpTestSimple(GenomicsTestData data) {
+        assertEquals(data.expeced, Genomics.optBottomUp(data.letters, data.words));
+    }
 
-	@Test
-	void testOptBottomUp() {
-		fail("Not yet implemented");
-	}
+    @Test
+    void optBottomUpTestBsp() {
+        assertEquals(4, Genomics.optBottomUp("CAGTCCAGTCAGTC", new String[]{"AGT", "CA", "CAG", "GTC", "TC", "TCA", "TCC"}));
+    }
 
-	@Test
-	void testOptTopDown() {
-		fail("Not yet implemented");
-	}
+    @Test
+    void optBottomUpTestComplex() {
+        assertEquals(0, Genomics.optBottomUp("AAAIGGGGTGIACTCTCCGAACATIICIIAAACICCTATGTCACCATCTAICTCICCIICGCAAIIGITCAGCGTIATIICICACA",
+                new String[]{"ITI", "CC", "CTG", "CCA", "IC", "CI", "TCITI", "AT"}));
+        assertEquals(80, Genomics.optBottomUp("GATCCTGCTGTCTCCTGTGCGATAGAGT", new String[]{"GAT", "GA", "TG", "AG", "TC", "TGC", "AGT", "TCC", "A", "G", "C"}));
+    }
 
-	@Test
-	void testMain() {
-		fail("Not yet implemented");
-	}
+    static Stream<GenomicsTestData> simpleTestSource(){
+        return Stream.of(new GenomicsTestData[]{
+            new GenomicsTestData(1, "", new String[]{""}),
+            new GenomicsTestData(1, "", new String[]{"A"}),
+            new GenomicsTestData(0, "A", new String[]{""}),
+            new GenomicsTestData(1, "A", new String[]{"A"}),
+            new GenomicsTestData(2, "AA", new String[]{"A", "AA"}),
+            new GenomicsTestData(4, "AABB", new String[]{"A", "AA", "B", "BB"})
+        });
+    }
 
+    static class GenomicsTestData{
+        public int expeced;
+        public String letters;
+        public String[] words;
+
+        public GenomicsTestData(int expected, String letters, String[] words){
+            this.expeced = expected;
+            this.letters = letters;
+            this.words = words;
+        }
+    }
 }
 
--- Blatt05/src/RowOfBowls.java
+++ Blatt05/src/RowOfBowls.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
 /**
  * This class implements a game of Row of Bowls.
  * For the games rules see Blatt05. The goal is to find an optimal strategy.
@@ -24,9 +27,43 @@ public class RowOfBowls {
      * @return number of game points that the first player gets, provided both parties play optimally
      */
     public int maxGainRecursive(int[] values) {
-        // TODO
+        if (values.length == 0) {
+            return 0;
+        }
+        if (values.length == 1) {
+            return values[0];
+        }
+        LinkedList<Integer> bowels = new LinkedList<Integer>();
+        for (int i : values) {
+            bowels.add(i);
+        }
+        int result = maxGainRecursive(bowels, 0, 0)[0];
+        System.err.println(bowels.toString() + result);
+        return result;
     }
 
+    private int[] maxGainRecursive(LinkedList<Integer> bowels, int player1, int player2) {
+        if (bowels.size() == 1) {
+            int[] i = {bowels.peekFirst() + player1, player2};
+            return i;
+        }
+        int tmp = bowels.removeFirst();
+        player1 += tmp;
+        int[] result = maxGainRecursive(bowels, player2, player1);
+        player1 -= tmp;
+        bowels.addFirst(tmp);
+        tmp = bowels.removeLast();
+        player1 += tmp;
+        int[] result2 = maxGainRecursive(bowels, player2, player1);
+        bowels.addLast(tmp);
+        if (result2[1] > result[1]) {
+            int output[] = {result2[1], result2[0]};
+            return output;
+        } else {
+            int output[] = {result[1], result[0]};
+            return output;
+        }
+    }
     
     /**
      * Calculates an optimal sequence of bowls using the partial solutions found in maxGain(int values)
