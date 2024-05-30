934e72805c11ad072f1698d23fb1b272b2d82e6d
hendrik_1409 <hendrik.schiele@gmx.de>
2024-05-30 22:41:28 +0200

    just for fun - use the inefficient function maxGain

--- Blatt05/src/RowOfBowls.java
+++ Blatt05/src/RowOfBowls.java
@@ -9,22 +9,36 @@ public class RowOfBowls {
 
     public RowOfBowls() {
     }
-    
+
     /**
      * Implements an optimal game using dynamic programming
+     * 
      * @param values array of the number of marbles in each bowl
-     * @return number of game points that the first player gets, provided both parties play optimally
+     * @return number of game points that the first player gets, provided both
+     *         parties play optimally
      */
-    public int maxGain(int[] values)
-    {
-        // TODO
+    public int maxGain(int[] values) {
+        if (values.length == 0) {
+            return 0;
+        }
+        if (values.length == 1) {
+            return values[0];
+        }
+        LinkedList<Integer> bowels = new LinkedList<Integer>();
+        for (int i : values) {
+            bowels.add(i);
+        }
+        int result[] = maxGainRecursive(bowels, 0, 0);
+        System.err.println(bowels.toString() + result);
+        return result[0] - result[1];
     }
 
     /**
      * Implements an optimal game recursively.
      *
      * @param values array of the number of marbles in each bowl
-     * @return number of game points that the first player gets, provided both parties play optimally
+     * @return number of game points that the first player gets, provided both
+     *         parties play optimally
      */
     public int maxGainRecursive(int[] values) {
         if (values.length == 0) {
@@ -44,7 +58,7 @@ public class RowOfBowls {
 
     private int[] maxGainRecursive(LinkedList<Integer> bowels, int player1, int player2) {
         if (bowels.size() == 1) {
-            int[] i = {bowels.peekFirst() + player1, player2};
+            int[] i = { bowels.peekFirst() + player1, player2 };
             return i;
         }
         int tmp = bowels.removeFirst();
@@ -57,28 +71,27 @@ public class RowOfBowls {
         int[] result2 = maxGainRecursive(bowels, player2, player1);
         bowels.addLast(tmp);
         if (result2[1] > result[1]) {
-            int output[] = {result2[1], result2[0]};
+            int output[] = { result2[1], result2[0] };
             return output;
         } else {
-            int output[] = {result[1], result[0]};
+            int output[] = { result[1], result[0] };
             return output;
         }
     }
-    
+
     /**
-     * Calculates an optimal sequence of bowls using the partial solutions found in maxGain(int values)
-     * @return optimal sequence of chosen bowls (represented by the index in the values array)
+     * Calculates an optimal sequence of bowls using the partial solutions found in
+     * maxGain(int values)
+     * 
+     * @return optimal sequence of chosen bowls (represented by the index in the
+     *         values array)
      */
-    public Iterable<Integer> optimalSequence()
-    {
+    public Iterable<Integer> optimalSequence() {
         // TODO
     }
 
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // For Testing
-        
-        }
-}
 
+    }
+}
