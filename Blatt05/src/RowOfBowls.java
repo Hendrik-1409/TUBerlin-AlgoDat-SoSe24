8460d85afddc6538e8980cf6443e75616e842282
hendrik_1409 <hendrik.schiele@gmx.de>
2024-05-31 11:47:06 +0200

    maxGain uses rewritte rekursive function. Prepeard vor Matrix - Storage integration

--- Blatt05/src/RowOfBowls.java
+++ Blatt05/src/RowOfBowls.java
@@ -7,6 +7,9 @@ import java.util.LinkedList;
  */
 public class RowOfBowls {
 
+    private int[][] solutionMatrix;
+    private int[] position;
+
     public RowOfBowls() {
     }
 
@@ -24,13 +27,35 @@ public class RowOfBowls {
         if (values.length == 1) {
             return values[0];
         }
-        LinkedList<Integer> bowels = new LinkedList<Integer>();
-        for (int i : values) {
-            bowels.add(i);
-        }
-        int result[] = maxGainRecursive(bowels, 0, 0);
-        System.err.println(bowels.toString() + result);
+        solutionMatrix = new int[values.length][values.length];
+        position = new int[] {-1, values.length};
+        int result[] = maxGain(values, 0, 0);
+        System.out.println(Arrays.toString(values) + Arrays.toString(result));
         return result[0] - result[1];
+    }
+
+    private int[] maxGain(int[] bowels, int player1, int player2) {
+        if (Math.abs(position[0] - position[1]) <= 1) {
+            int[] i = {player1, player2};
+            return i;
+        }
+        position[0]++;
+        player1 += bowels[position[0]];
+        int[] result = maxGain(bowels, player2, player1);
+        player1 -= bowels[position[0]];
+        position[0]--;
+        position[1]--;
+        player1 += bowels[position[1]];
+        int[] result2 = maxGain(bowels, player2, player1);
+        player1 -= bowels[position[1]];
+        position[1]++;
+        if (result[1] < result2[1]) {
+            int output[] = { result2[1], result2[0] };
+            return output;
+        } else {
+            int output[] = { result[1], result[0] };
+            return output;
+        }
     }
 
     /**
@@ -52,7 +77,7 @@ public class RowOfBowls {
             bowels.add(i);
         }
         int result[] = maxGainRecursive(bowels, 0, 0);
-        System.err.println(bowels.toString() + result);
+        System.out.println(bowels.toString() + Arrays.toString(result));
         return result[0] - result[1];
     }
 
